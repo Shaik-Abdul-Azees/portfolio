a092a38e631a53d48c5c98a1db3b7b7f1db9263d
shaik abdul azees <abdulazeesshaik21@gmail.com>
2026-04-09 10:00:57 +0530

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
 
 import Contact from './components/Contact/Contact';
-import Chatbot from './components/Chatbot/Chatbot';
 import Footer from './components/Footer/Footer';
 import './App.css'; // You can remove this line if App.css is no longer needed
 
@@ -24,7 +23,6 @@ function App() {
         <Contact />
       </main>
       <Footer />
-      <Chatbot />
     </div>
   );
 }
